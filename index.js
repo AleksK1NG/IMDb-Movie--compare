19a1a4bfe1118727564ccc91f93ebd18bff4b002
AlexBryksin <alexander.bryksin@yandex.ru>
2019-12-30 18:21:06 +0300

    add debounce func

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 console.log('Hi there! ')
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   try {
     const response = await axios.get('http://www.omdbapi.com/', {
       params: {
         apikey: 'd7a0924e',
-        s: 'avengers',
+        // i: 'tt0848228',
+        s: searchTerm,
       },
     })
 
@@ -14,4 +15,23 @@ const fetchData = async () => {
   }
 }
 
-fetchData()
+const input = document.querySelector('input')
+
+const debounce = (fn, delay = 300) => {
+  let timeoutId
+
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(() => {
+      fn.apply(null, args)
+    }, delay)
+  }
+}
+
+const onInput = (e) => {
+  fetchData(e.target.value)
+}
+
+input.addEventListener('input', debounce(onInput, 300))
