c7b111d54d5ff44c12b1e7ef25fc86a2db6ad36e
AlexBryksin <alexander.bryksin@yandex.ru>
2019-12-31 12:13:16 +0300

    add render single movie

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log('Hi there! ')
 const fetchData = async (searchTerm) => {
   try {
     const response = await axios.get('http://www.omdbapi.com/', {
@@ -58,6 +57,13 @@ const onInput = async (e) => {
       <h1>${movie.Title}</h1>
     `
 
+    // click on dropdown menu item
+    option.addEventListener('click', (e) => {
+      dropdown.classList.remove('is-active')
+      input.value = movie.Title
+      onMovieSelect(movie)
+    })
+
     resultsWrapper.appendChild(option)
   }
 }
@@ -66,8 +72,43 @@ input.addEventListener('input', debounce(onInput, 300))
 
 // close autocomplete dropdown
 document.addEventListener('click', (e) => {
-  console.log(e.target)
   if (!root.contains(e.target)) {
     dropdown.classList.remove('is-active')
   }
 })
+
+// request for single movie on select from autocomplete dropdown
+const onMovieSelect = async (movie) => {
+  try {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'd7a0924e',
+        i: movie.imdbID,
+      },
+    })
+    console.log(' response => ', response.data)
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+// render movie
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media">
+    <figure class="media-left">
+    <p class="image">
+    <img src="${movieDetail.Poster}" alt="Poster">
+    </p>
+    </figure>
+        <div class="media-content">
+        <div class="content">
+        <h1>${movieDetail.Title}</h1>
+        <h4>${movieDetail.Genre}</h4>
+        <p>${movieDetail.Plot}</p>
+    </div>
+    </div>
+    </article>
+  `
+}
